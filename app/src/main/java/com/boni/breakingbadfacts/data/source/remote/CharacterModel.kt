1c70272f6233cdefb8cc6a460f6ce28f26f926f0
renan.boni <renan.boni@usp.br>
2019-12-22 19:35:56 -0300

    Added Character Model

---
+++ app/src/main/java/com/boni/breakingbadfacts/data/source/remote/CharacterModel.kt
@@ -0,0 +1,18 @@
+package com.boni.breakingbadfacts.data.source.remote
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class CharacterModel(
+    @SerializedName("char_id") val id: Long? = null,
+    @SerializedName("name") val name: String? = null,
+    @SerializedName("birthday") val birthday: String? = null,
+    @SerializedName("occupation") val occupation: ArrayList<String>? = null,
+    @SerializedName("img") val img: String? = null,
+    @SerializedName("status") val status: String? = null,
+    @SerializedName("appearance") val appearance: ArrayList<Int>? = null,
+    @SerializedName("nickname") val nickname: String? = null,
+    @SerializedName("portrayed") val portrayed: String? = null
+) : Parcelable
