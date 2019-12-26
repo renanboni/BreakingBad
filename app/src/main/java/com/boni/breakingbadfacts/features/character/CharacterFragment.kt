02b4b7de9d7fb15df14f00e5b7528d087127204e
renan.boni <renan.boni@usp.br>
2019-12-26 19:30:33 -0300

    Fixing colors

--- app/src/main/java/com/boni/breakingbadfacts/features/character/CharacterFragment.kt
+++ app/src/main/java/com/boni/breakingbadfacts/features/character/CharacterFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.boni.breakingbadfacts.R
@@ -67,6 +66,7 @@ class CharacterFragment : HasViewModel<CharacterViewModel>, BaseFragment() {
                 skipLastLine = true
             )
         )
+        executionLabel.setVisible(viewState.deaths.isNotEmpty())
     }
 
     @SuppressLint("SetTextI18n")
